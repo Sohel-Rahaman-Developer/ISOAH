cc06eb22ad879aae007855982bb8fec7e6c9b30e
soheldeveloper <sohelrahaman.official@gmail.com>
2025-03-24 11:54:59 +0530

    recent project done

--- app/components/RecentProjects.tsx
+++ app/components/RecentProjects.tsx
@@ -31,7 +31,7 @@ const RecentProjects = () => {
   };
 
   return (
-    <section className=" text-[#ddd] overflow-hidden">
+    <section className="text-[#ddd] overflow-hidden">
       <div className="py-10 max-w-7xl mx-auto px-3">
         <div className="max-w-7xl mx-auto">
           <h1 className="heading text-[30px] font-bold md:text-[38px] text-center mb-0 md:mb-10">
@@ -40,8 +40,8 @@ const RecentProjects = () => {
           <div className="flex flex-wrap items-center justify-around px-4 gap-10 sm:gap-5 md:gap-16 mt-5">
             {projects.map((item, index) => (
               <motion.div
-                className="lg:min-h-[32.5rem] h-[22rem] md:h-[25rem] backdrop-blur-sm flex items-center justify-center sm:w-64 md:w-80 w-[100%]"
                 key={item.id}
+                className="lg:min-h-[32.5rem] h-[22rem] md:h-[25rem] backdrop-blur-sm flex items-center justify-center sm:w-64 md:w-80 w-[100%]"
                 custom={getDirection(index)}
                 initial="hidden"
                 whileInView="visible"
@@ -52,19 +52,26 @@ const RecentProjects = () => {
                 }}
               >
                 <PinContainer
-                  title="sohelrahaman.ui.com"
-                  href="https://twitter.com/sohelrahaman"
+                  title={"forensiqhq.com"}
+                  href={item.link}
                 >
                   <div className="relative flex items-center justify-center sm:w-[40vw] md:w-80 overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                     <div
                       className="relative w-72 h-full overflow-hidden rounded-3xl"
                       style={{ backgroundColor: "#13162D" }}
                     >
-                      <Image width={500}
-                        height={500} src="/bg.png" alt="bgimg" className="rounded-md"/>
+                      <Image
+                        width={500}
+                        height={500}
+                        src="/bg.png"
+                        alt="bgimg"
+                        className="rounded-md"
+                      />
                     </div>
-                    <Image width={500}
-                      height={500} src={item.img}
+                    <Image
+                      width={500}
+                      height={500}
+                      src={item.img}
                       alt="cover"
                       className="z-0 absolute rounded-md bottom-0"
                     />
@@ -76,30 +83,12 @@ const RecentProjects = () => {
 
                   <p
                     className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                    style={{
-                      color: "#BEC1DD",
-                      margin: "1vh 0",
-                    }}
+                    style={{ color: "#BEC1DD", margin: "1vh 0" }}
                   >
                     {item.des}
                   </p>
 
                   <div className="flex items-center justify-between mt-7 mb-3">
-                    {/* <div className="flex items-center">
-                      {item.iconLists.map((icon, idx) => (
-                        <div
-                          key={idx}
-                          className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                          style={{
-                            transform: `translateX(-${5 * idx + 2}px)`,
-                          }}
-                        >
-                          <Image   width={500}
-  height={500}src={icon} alt="icon" className="p-2" />
-                        </div>
-                      ))}
-                    </div> */}
-
                     <div className="flex justify-center items-center">
                       <p className="flex lg:text-xl md:text-xs text-sm text-green-400">
                         Check Live Site
